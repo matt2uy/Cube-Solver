0d93bb15943a669921e0ceb9a5a4d64be77e7925
matt2uy <matt2uy@gmail.com>
2014-04-16 17:03:37 -0400

    (found a way to input strings from serial.read())

--- Serial_Comm/Bulk_Parse/Receive_Parse.cpp
+++ Serial_Comm/Bulk_Parse/Receive_Parse.cpp
@@ -14,7 +14,7 @@ char red_face[9];
 char green_face[9];
 char orange_face[9];
 
-String raw_cube_string = "abcdefghi";
+String raw_cube_string = "";
 
 void receive_raw_cube()
 {
@@ -24,36 +24,55 @@ void receive_raw_cube()
 	while(Serial.read() != ready_signal)
 	{
 		Serial.println(ready_signal);
+		delay(10);
 	}
 			
-
+	Serial.println("here");
 	// receive color
 	//raw_cube_string = Serial.read();
 	//char string = Serial.read();
-	while(true)
-	{
-		Serial.println("string");
-	}
 }
 
-void parse_raw_cube()
+void accept_string()
+{
+	while(raw_cube_string == "")
+	{
+		char character;
+		while(Serial.available()) 
+		{
+		    character = Serial.read();
+		   	raw_cube_string.concat(character);
+		}
+		delay(100);
+	}
+	Serial.print("String Accepted: ");
+	Serial.print(raw_cube_string);
+}
+
+void parse_raw_cube() // need an input of 54 char length string
 {	// convert to char array
+	Serial.print("Parsing... ");
 	raw_cube_string.toCharArray(yellow_face, sizeof(yellow_face));
+	Serial.println("yellow...");
 	for(int x = 0; x < 9; x++)
 	{
 		yellow_face[x] = raw_cube_string[x];
-  		Serial.println(raw_cube_string[x]);
+  		Serial.println(yellow_face[x]);
 	}
+	/*Serial.println("white...");
+	for(int x = 9; x < 18; x++)
+	{
+		white_face[x-9] = raw_cube_string[x];
+  		Serial.println(white_face[x-9]);
+	}*/
 }
 
 
 void loop()
 {
-	receive_raw_cube();
-	//parse_raw_cube();
-	
-	while(true)
-	{
-		// do nothing
-	}
+
+	accept_string();
+
+	parse_raw_cube();
+	while(true){}
 }
